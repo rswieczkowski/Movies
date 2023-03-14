3f44e06dc83f55adeea3e562219d825b1513d86f
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2023-03-14 09:33:44 +0100

    Second commit

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+
+    #[Route('/movies', name: 'app_movies')]
+    public function index(): Response
+    {
+        $movies = ['Avengers: Endgame', 'Inception', 'Godfather I', 'Godfather II', 'Godfather III' ];
+        return $this->render('index.html.twig', [
+            'movies'=>$movies
+        ]);
+    }
+}
