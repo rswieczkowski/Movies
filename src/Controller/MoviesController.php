5ff26d8f3c36ce7de21c30d54fa1375952bc2d9f
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2023-03-15 23:43:13 +0100

    repository

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +13,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class MoviesController extends AbstractController
 {
 
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
-        $movies = ['Avengers: Endgame', 'Inception', 'Godfather I', 'Godfather II', 'Godfather III' ];
-        return $this->render('index.html.twig', [
-            'movies'=>$movies
-        ]);
+        $repository = $this->em->getRepository(Movie::class);
+        $movies = $repository->getClassName();
+        dd($movies);
+        return $this->render('index.html.twig');
     }
 }
