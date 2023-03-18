858039a376d4beed329a6d6bad52bc7954f144f6
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2023-03-18 11:34:22 +0100

    reading - seecond commit

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -13,22 +13,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class MoviesController extends AbstractController
 {
 
-    private EntityManagerInterface $em;
+    private $movieRepository;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(MovieRepository $movieRepository)
     {
-        $this->em = $em;
+        $this->movieRepository = $movieRepository;
     }
 
-    #[Route('/movies', name: 'app_movies')]
+
+    #[Route('/movies', name: 'movies', methods: ['GET'])]
     public function index(): Response
     {
-        $repository = $this->em->getRepository(Movie::class);
-        $movies = $repository->getClassName();
+        $movies = $this->movieRepository->findAll();
 
-        return $this->render('index.html.twig');
+        return $this->render('movies/index.html.twig', ['movies' => $movies]);
+    }
 
+    #[Route('/movies/{id}', name: 'movie', methods: ['GET'])]
+    public function show($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
 
+        return $this->render('movies/show.html.twig', ['movie' => $movie]);
     }
 
 }
